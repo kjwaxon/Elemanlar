e9975ddc84a9bb3c0df460cc388204014c4435ff
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:37:36 +0300

    Consoleappeleman

---
+++ ConsoleAppEleman/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleAppEleman
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            ArrayList elemanlar = new ArrayList();
+
+            string x;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write((i + 1) + ".ismi giriniz :");
+                x = Console.ReadLine();
+                elemanlar.Add(x);
+
+            }
+            Console.WriteLine("Elemanlar ekrana yazılıyor");
+            for (int i =0;i <5;i++)
+            {
+                Console.WriteLine((i + 1) + ".isim {0}", elemanlar[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
